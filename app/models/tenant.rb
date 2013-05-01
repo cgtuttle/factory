c3c740a387cd26a27cadde43c5a2265c03936a4a
Carl Tuttle <cgtuttle@gmail.com>
2013-05-01 17:56:12 -0500

    Basic Cancan functionality complete

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -7,5 +7,13 @@ class Tenant < ActiveRecord::Base
   attr_accessible :name
 
   cattr_accessor :current_id
+
+  def self.current_id=(id)
+    Thread.current[:tenant_id] = id
+  end
+  
+  def self.current_id
+    Thread.current[:tenant_id]
+  end
   	
 end

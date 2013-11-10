b4f9bfb9f3cb5618f2c2efc1a9e4c9436e378b76
cgtuttle <cgtuttle@gmail.com>
2013-11-10 10:01:31 -0600

    Added escape condition for role.display_order = nil

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,7 @@
 class Role < ActiveRecord::Base
 	has_many :memberships
   attr_accessible :role_name, :display_order
-  scope :for_list, where(:viewable => true ).order(display_order)
+  scope :for_list, where(:viewable => true ).order(display_order || 0)
 
 
 end

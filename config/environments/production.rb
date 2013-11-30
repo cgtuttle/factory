c326cb0fec2b24fab25d1b89285a4090c0a624e0
cgtuttle <cgtuttle@gmail.com>
2013-11-30 10:49:18 -0600

    changed precompile parameters

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Factory::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Force precomplie of images
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg]
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
